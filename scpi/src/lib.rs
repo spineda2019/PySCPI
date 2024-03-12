6d63fdf5fa3c5e0a8963e987be40b632c84e71a5
spineda2019 <spineda@wpi.edu>
2024-03-12 12:18:55 -0400

    simplify networking using Messenger struct

--- scpi/src/lib.rs
+++ scpi/src/lib.rs
@@ -18,49 +18,20 @@ mod messenger;
 mod networking;
 mod unit_tests;
 
-use std::{
-    io::{Error, ErrorKind, Write},
-    net::{IpAddr, SocketAddr, TcpStream, UdpSocket},
-    str::FromStr,
-};
+use std::{io::Error, net::IpAddr};
 
 use messenger::Messenger;
 use networking::NetworkMode;
 
 pub fn send_scpi_message(
     message: &str,
-    mode: &NetworkMode,
+    mode: NetworkMode,
     remote_client: &IpAddr,
     remote_port: u16,
     local_port: u16,
 ) -> Result<usize, Error> {
-    let clean_message: String = format!("{}\r\n", message.trim());
-    let scpi_message: &[u8] = clean_message.as_bytes();
-    let local_host: IpAddr = match IpAddr::from_str("0.0.0.0") {
-        Ok(x) => x,
-        Err(_) => {
-            let msg: &str = "Creating localhost object failed...";
-            eprint!("{}", msg);
-            return Err(Error::new(ErrorKind::Interrupted, msg));
-        }
-    };
-    let local_address = SocketAddr::new(local_host, local_port);
-
-    let remote_address = SocketAddr::new(*remote_client, remote_port);
-
-    match mode {
-        NetworkMode::Udp => {
-            let local_socket: UdpSocket = UdpSocket::bind(local_address)?;
-            Ok(local_socket.send_to(scpi_message, remote_address)?)
-        }
-        NetworkMode::Tcp => {
-            let mut local_socket: TcpStream = TcpStream::connect(remote_address)?;
-            local_socket.write_all(scpi_message)?;
-            Ok(scpi_message.len())
-        }
-        NetworkMode::UdpMulticast => todo!(),
-        NetworkMode::TcpMulticast => todo!(),
-    }
+    let mut messenger: Messenger = Messenger::new(local_port, remote_port, remote_client, mode)?;
+    messenger.send_message(message)
 }
 
 pub fn send_repeated_scpi_message(
@@ -71,19 +42,18 @@ pub fn send_repeated_scpi_message(
     local_port: u16,
     repititions: Option<usize>,
 ) -> Result<usize, Error> {
-    let messenger: Messenger = Messenger::new(local_port, remote_port, remote_client, mode)?;
+    let mut messenger: Messenger = Messenger::new(local_port, remote_port, remote_client, mode)?;
 
     match repititions {
         Some(number) => {
-            let result: usize = 0;
+            let mut result: usize = 0;
             for _ in 0..number {
-                // This is not as good for performance
-                todo!();
+                result = messenger.send_message(message)?;
             }
             Ok(result)
         }
         None => loop {
-            todo!()
+            messenger.send_message(message)?;
         },
     }
 }

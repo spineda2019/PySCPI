953a272b7449a0ff6211f098c43b1b1bfe999bef
spineda2019 <spineda@wpi.edu>
2024-03-12 11:03:21 -0400

    implement cargo fix suggestion

--- python_bindings/src/lib.rs
+++ python_bindings/src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 
 #[pymodule]
-fn scpi_sender_pylib(_py: Python, m: &PyModule) -> PyResult<()> {
+fn scpi_sender_pylib(_py: Python, _m: &PyModule) -> PyResult<()> {
     Ok(())
 }

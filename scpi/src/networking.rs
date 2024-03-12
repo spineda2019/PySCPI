470a290cd1eb83e16290c00ff76ddabd449c5022
spineda2019 <spineda@wpi.edu>
2024-03-12 12:07:32 -0400

    add network sending enum

--- scpi/src/networking.rs
+++ scpi/src/networking.rs
@@ -1,6 +1,13 @@
+use std::net::{TcpStream, UdpSocket};
+
 pub enum NetworkMode {
     Udp,
     Tcp,
     UdpMulticast,
     TcpMulticast,
+}
+
+pub enum NetworkSender {
+    Udp(UdpSocket),
+    Tcp(TcpStream),
 }
